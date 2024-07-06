bd34e6570d23ddfac47c0c66b6b2504f73076eb6
jrnerupudinho <atmuri.sai11@gmail.com>
2024-07-06 12:14:27 -0400

    new features

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const path = require('path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin')
+app.set('view engine', 'pug'); //pug is supported by express 
+app.set('views', 'views');
+
+const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
 // app.use((req,res,next) => {
@@ -19,10 +22,12 @@ const shopRoutes = require('./routes/shop')
 app.use(bodyParser.urlencoded({extended: false})); //registers the middleware and calls the next in the end. This allows normal forms alone but not files 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'views','page-not-found.html'))});
+    // res.status(404).sendFile(path.join(__dirname,'views','page-not-found.html'))
+    res.status(404).render('page-not-found',{pageTitle:'Page Not Found'});
+});
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,14 @@ const express = require('express');
 const path = require('path')
 
 const rootDir = require('../helper/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 
+
 router.get('/',(req,res,next) => {
+    console.log(adminData.products)
     res.sendFile(path.join(rootDir, 'views','shop.html')); //This is the way to use by adding files, path join detects the os and runs accordingly, dirname gives the path that the file that we are using
 }); //this is a middleware
 
